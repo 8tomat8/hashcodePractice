cae466a8d5dd1bf91b6cbcd9670d6d1edb41a4cd
Bogdan Prodan <Bogdan.Prodan@eleks.com>
2018-02-25 12:04:25 +0200

    max elems

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,22 @@
 package app
+
+var r = 100
+var c = 100
+var tomatoes = true
+var mushrooms = false
+
+func maxSliceCount(input [r][c]bool) int {
+	t := 0
+	arraySize := r * c
+	for _, v := range input {
+		for _, v1 := range v {
+			if v1 {
+				t++
+			}
+		}
+	}
+	if arraySize-t > t {
+		return t
+	}
+	return arraySize - t
+}
